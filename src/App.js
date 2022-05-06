2f06aa37c8295d019b735d762695c98edc1b3a21
emte <emte@itu.dk>
2022-05-07 00:21:13 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+var ua = navigator.userAgent || navigator.vendor || window.opera;
+
+function isFacebookApp() {
+    return (ua.indexOf("FBAN") > -1) || (ua.indexOf("FBAV") > -1);
+
+}
+
+if (isFacebookApp()) {
+  if (!window.location.href.match('redirect_fb')) {
+    console.log("true")
+    window.location.href = "https://example.com/redirect_fb";
+  }
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hi there</h1>
       </header>
     </div>
   );
